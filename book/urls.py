14cc4b875c6059aed643b8519f1cf2e19e779e1f
martina <martinagracy28@gmail.com>
2020-05-19 12:30:51 +0530

    final

--- book/urls.py
+++ book/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-path("home",views.home,name="home"),
+path("",views.home,name="home"),
 path("slambook",views.slambook,name="slambook"),
 path('thank',views.thank,name='thank'),
 
